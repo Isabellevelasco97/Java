e8b5d60ac374637f356b0bd99839d08c4255cb6b
Isabelle Velasco <isabelle.velasco97@gmail.com>
2020-03-11 14:37:54 -0700

    assignment on loops and random

--- src/assignment2/Assignment2.java
+++ src/assignment2/Assignment2.java
@@ -9,11 +9,10 @@ public class Assignment2 {
 		// TODO Auto-generated method stub
 
 		// Initializing variables
-		int high = 100;
-		int low = 1;
+		int low = 0;
 		int numGuess = 0;
 		Random ran = new Random();
-		
+
 		Scanner cin = new Scanner(System.in);
 
 		System.out.println("Pick a number between 1 and 100");
@@ -22,52 +21,60 @@ public class Assignment2 {
 		System.out.println("Is the number you picked " + 50 + "?");
 		String answer = cin.nextLine();
 		numGuess++;
-		//if guess = 50 or != 50
+		// if guess = 50 or != 50
 		if (answer.equalsIgnoreCase("yes")) {
 			System.out.println("I WIN!!" + "\nNumber of guesses: " + numGuess);
 			System.exit(0);
 		} else {
 			System.out.println("Is it less than " + 50 + "?");
 			answer = cin.nextLine();
-		//if guess < 50 or > 50
+			// if guess < 50 or > 50
 			if (answer.equalsIgnoreCase("yes")) {
-				high = 50;
 				System.out.println("Is the number you picked " + 25 + "?");
 				answer = cin.nextLine();
 				numGuess++;
-				//if guess = 25 or != 25
+				// if guess = 25 or != 25
 				if (answer.equalsIgnoreCase("yes")) {
 					System.out.println("I WIN!!" + "\nNumber of guesses: " + numGuess);
-				}
-				// checking if guess is < 25 or > 25
-				else {
-					System.out.println("Is it less than " + 25 + "?");
+					System.exit(0);
+				} else {
+					System.out.println("Is it less than " + 25 + "?"); // checking if guess is < 25 or > 25
 					answer = cin.nextLine();
 					// if guess < 25
-					if (answer.equalsIgnoreCase("yes")) {
-					high = 25;
-					while (numGuess < 5) {
-						numGuess += 1;
-					
-					System.out.println("Is the number you picked " + (ran.nextInt(25) + low) + "?");
+					if (answer.equalsIgnoreCase("yes")) {	
+					} else {
+						low = 25;
+					}
+				}
+			// is guess = 75 or != 75
+			} else {
+				System.out.println("Is the number you picked " + 75 + "?");
+				answer = cin.nextLine();
+				numGuess++;
+				if (answer.equalsIgnoreCase("yes")) {
+					System.out.println("I WIN!!" + "\nNumber of guesses: " + numGuess);
+					System.exit(0);
+				} else {
+					System.out.println("Is it less than " + 75 + "?"); // checking if guess is < 75 or > 75
 					answer = cin.nextLine();
 					if (answer.equalsIgnoreCase("yes")) {
-						System.out.println("I WIN!!" + "\nNumber of guesses: " + numGuess);
+						low = 50;
+					} else {
+						low = 75;
 					}
-					}
-					System.out.println("Aaah OK you win :(" + "\nNumber of guesses: " + numGuess);
-					System.exit(0);
-					}
-			 //else {
-				low = 50;
-				System.out.println("Is the number you picked " + 75 + "?");
-
+				}
 			}
-
 		}
-
-	}
-
+		while (numGuess < 5) {
+			numGuess += 1;
+			System.out.println("Is the number you picked " + (ran.nextInt(24) + low + 1) + "?");
+			answer = cin.nextLine();
+			if (answer.equalsIgnoreCase("yes")) {
+				System.out.println("I WIN!!" + "\nNumber of guesses: " + numGuess);
+				System.exit(0);
+			}
+		}
+		System.out.println("Aaah OK you win :(" + "\nNumber of guesses: " + numGuess);
+		cin.close();
 	}
 }
-
