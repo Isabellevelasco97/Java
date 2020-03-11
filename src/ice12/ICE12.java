e1b3208930dc7d1caabcc5a4549a40340a2e07e9
Isabelle Velasco <isabelle.velasco97@gmail.com>
2020-03-11 14:41:14 -0700

    practice on arrays and random

--- src/ice12/ICE12.java
+++ src/ice12/ICE12.java
@@ -7,31 +7,47 @@ public class ICE12 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Part 1 Iterating over an array with for loop
+		
+		// I.Create an array of integers of lenght 100 int[ ] nums = new int[100];
+		// called nums II.Create a String variable called arrStr. We want to construct a
+		// string that represents the array with this rule: If the number in the array
+		// is a multiple of 7, the item will be replaced with the word "LUCKY" III.Use
+		// the class Random and assign a random number to each empty cell in nums. Use a
+		// for loop with upper limit of arr.length in your loop condition. IV.If the
+		// array contains 10, 13, 21, 80 ... we want arrStr to be "[10, 13, LUCKY, 80
+		// ...]" V.So you should first assign '[' to the arrStr variable, then loop
+		// through the array and add each number to the string variable VI.IF the number
+		// is a multiple of 7 arr[i] % 7 == 0 you should append "LUCKY" to arrStr
+		
 		int[] nums = new int[100];
 		String arrStr = "[";
-		Random ran = new Random();
+		Random rand = new Random();
 		
 		for (int i = 0; i < nums.length; i++) {
-			nums[i] = ran.nextInt(100);
+			nums[i] = rand.nextInt(100);
 			if ((nums[i]%7)==0) {
-				arrStr += "LUCKY";
-			}
-			else {
-				
+				arrStr += " LUCKY ";
+			} else {
 				arrStr += (nums[i]);
 			}
-			arrStr += ", ";
+			arrStr += " , ";
 		}
-		System.out.println(arrStr + "]");
-		//[64, LUCKY, LUCKY, 27, 66, 6, LUCKY, 95, 75, 76, 94, 4, 30, 22, 87, 47, 3, 11, 90, 25, 27, 79, LUCKY, LUCKY, 24, LUCKY, LUCKY, 2, 68, 6, 26, 96, 85, 34, 55, 22, 73, 92, LUCKY, 89, 83, 20, 6, LUCKY, 62, 53, 76, 81, 27, 45, 16, 22, 90, 4, 39, 26, 15, 64, 92, 85, 25, 38, 87, 54, 72, 27, 76, LUCKY, 33, 34, 24, LUCKY, LUCKY, 66, 96, 17, 25, 3, 46, 20, 33, 43, 17, 78, 17, 83, 20, 51, 78, 36, 51, 59, 51, 48, 65, LUCKY, 36, 18, 52, 8, ]
-
-		// Part 2 Random Characters
-		char[] arrChar = new char[200];
-		for (int i = 0; i < arrChar.length; i++) {
-			arrChar [i] = (char)(ran.nextInt(245)+10);
-			System.out.print(arrChar[i]);
+		System.out.print(arrStr + "\n");
+		
+		
+		
+		// I.Create an array of chars of length 200 II.Use a for loop and iterate over the array and use
+		// the random object to create random numbers between 10 and 255. You can use
+		// rand.nextInt(245) + 10 III.Cast the number into a char like this (char) (10 +
+		// rand.nextInt(245)) and assign it to the cells inside the array IV.After the
+		// loop is done, loop through the char array in a separate for loop and print
+		// each character in the array with System.out.print()
+		
+		char[] chArray = new char[200];
+		for (int i = 0; i < chArray.length; i++) {
+			chArray[i] = (char) (10 + rand.nextInt(245));
+			System.out.print(chArray[i]);
 		}
-		// Prints out ú^Îß3¦aÀ)f¦hdCË´x±tãåP|Z"½ÁqÙ³Ý&»Ùãrão=?-FyU43R[<éü'`ÓÁ'Ôb­å1æg»´ÚÝHÄ1OàmVq(SqQ6^·¾5{_ã;ï÷×¨â}OZÃzX;¯¦V:@.[Ë|¦þ'zØ£{kÍÃr¬Îcº#³~jHÕÔÐ]w¸0|÷'KEvyÍ2,DíÔ|»º
+		
 	}
-
 }
